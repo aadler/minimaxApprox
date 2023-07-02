0ec2b3db2b22f1b340d6159530d951faab38f844
Avraham Adler <avraham.adler@mcgillpartners.com>
2023-07-02 10:21:58 +0300

    1) Use more robust check for minimzation/maximization and remove need for eps in remPoly. Will do remRat later. 2) Add license code 3) is Oscil and polyCalc helper functions 4) Add decorations to remRat for plotting 5) REMRAT ON HOLD FOR NOW

--- R/Remez.R
+++ R/Remez.R
@@ -1,3 +1,6 @@
+# Copyright Avraham Adler (c) 2023
+# SPDX-License-Identifier: MPL-2.0+
+
 chebNodes <- function(n, a, b) {
   zapsmall(
     sort(0.5 * (a + b + (b - a) * cos((2 * seq_len(n) - 1) * pi / (2 * n))))
@@ -5,20 +8,21 @@ chebNodes <- function(n, a, b) {
 }
 
 vanderMat <- function(x, n) {
-  n1 <- n + 1
-  matrix(rep(x, each = n1) ^ (seq_len(n1) - 1), ncol = n1, byrow = TRUE)
+  n1 <- n + 1L
+  matrix(rep(x, each = n1) ^ (seq_len(n1) - 1L), ncol = n1, byrow = TRUE)
 }
 
 callFun <- function(fn, x) {
-  if (is.character(fn)) {
-    y <- do.call(match.fun(eval(parse(text = fn))), args = list(x = x))
-  } else if (is.function(fn)) {
-    y <- do.call(match.fun(fn), args = list(x = x))
+  if (is.function(fn)) {
+    return(do.call(match.fun(fn), args = list(x = x)))
   } else {
-    stop("Unable to parse function")
+    stop("Unable to parse function.")
   }
-  y
 }
+
+isOscil <- function(x) all(abs(diff(sign(x))) == 2)
+
+polyCalc <- function(x, a) sum(a * x ^ (seq_along(a) - 1L))
 
 remPolyMat <- function(x) {
   n <- length(x)
@@ -28,15 +32,13 @@ remPolyMat <- function(x) {
 
 remPolyCoeffs <- function(x, fn) {
   PP <- solve(remPolyMat(x), callFun(fn, x))
-  list(b = PP[-length(PP)], E = abs(PP[length(PP)]))
+  list(b = PP[-length(PP)], E = PP[length(PP)])
 }
 
-remPolyErr <- function(x, b, fn) {
-  sum(b * x ^ (seq_along(b) - 1)) - callFun(fn, x)
-}
+remPolyErr <- function(x, b, fn) polyCalc(x, b) - callFun(fn, x)
 
 remPolyRoots <- function(x, b, fn) {
-  r <- double(length(x) - 1)
+  r <- double(length(x) - 1L)
   for (i in seq_along(r)) {
     r[i] <- uniroot(remPolyErr, lower = x[i], upper = x[i + 1L],
                     b = b, fn = fn)$root
@@ -44,21 +46,26 @@ remPolyRoots <- function(x, b, fn) {
   r
 }
 
-remPolySwitch <- function(r, l, u, b, fn, eps) {
+remPolySwitch <- function(r, l, u, b, fn) {
   nodes <- data.frame(lower = c(l, r), upper = c(r, u))
-  x <- double(length(r) + 1)
+  x <- double(length(r) + 1L)
+  maximize <- sign(remPolyErr(l, b, fn)) == 1
   for (i in seq_along(x)) {
     x[i] <- optimize(remPolyErr,
                      lower = nodes$lower[i],
                      upper = nodes$upper[i],
                      b = b, fn = fn,
-                     maximum = remPolyErr(nodes$lower[i] + eps,
-                                      b = b, fn = fn) > 0)[[1]]
+                     maximum = maximize)[[1L]]
+    maximize <- !maximize
+  }
+  errs <- sapply(x, remPolyErr, b = b, fn = fn)
+  if (!isOscil(errs)) {
+    stop("Control points are not oscillating in sign.\n", x)
   }
   x
 }
 
-remPoly <- function(fn, lower, upper, degree, showProgress = FALSE, opts = list()) {
+remPoly <- function(fn, lower, upper, degree, opts = list()) {
 
   # Handle configuration options
   if ("maxiter" %in% names(opts)) {
@@ -67,16 +74,16 @@ remPoly <- function(fn, lower, upper, degree, showProgress = FALSE, opts = list(
     maxiter <- 2500
   }
 
+  if ("showProgress" %in% names(opts)) {
+    maxiter <- opts$showProgress
+  } else {
+    showProgress <- FALSE
+  }
+
   if ("tol" %in% names(opts)) {
     tol <- opts$tol
   } else {
-    tol <- sqrt(.Machine$double.eps)
-  }
-
-  if ("eps" %in% names(opts)) {
-    tol <- opts$eps
-  } else {
-    eps <- min((upper - lower) / 1000, 1e-3)
+    tol <- 1e-12
   }
 
   # Initial x's
@@ -92,7 +99,7 @@ remPoly <- function(fn, lower, upper, degree, showProgress = FALSE, opts = list(
     }
     i <- i + 1L
     r <- remPolyRoots(x, PP$b, fn)
-    x <- remPolySwitch(r, lower, upper, PP$b, fn, eps)
+    x <- remPolySwitch(r, lower, upper, PP$b, fn)
     PP <- remPolyCoeffs(x, fn)
     errs <- sapply(x, remPolyErr, b = PP$b, fn = fn)
     abserrs <- abs(errs)
@@ -100,8 +107,8 @@ remPoly <- function(fn, lower, upper, degree, showProgress = FALSE, opts = list(
 
     if (all(diff(abserrs) < tol) &&
         all(abs(diff(sign(errs))) == 2) &&
-        (max(abserrs) <= PP$E ||
-         isTRUE(all.equal(max(abserrs), PP$E, tol = tol)))) break
+        (max(abserrs) <= abs(PP$E) ||
+         isTRUE(all.equal(max(abserrs), abs(PP$E), tol = tol)))) break
   }
   ret <- list(b = PP$b, E = PP$E)
   attr(ret, "type") <- "Polynomial"
@@ -129,17 +136,18 @@ remRatCoeffs <- function(x, E, fn, nD, dD) {
   P <- solve(remRatMat(x, E, y, nD, dD), y)
   RR <- list(a = P[1:(nD + 1L)],
              b = c(1, P[(nD + 2L):(nD + dD + 1L)]),
-             E = abs(P[length(P)]))
+             E = P[length(P)])
   if (sum(sapply(RR, length)) != (length(x) + 1)) {
     stop("Catastrophic Error. Result vector not of required length.")
   }
   RR
 }
 
-remRatErr <- function(x, a, b, fn) {
-  sum(a * x ^ (seq_along(a) - 1)) / sum(b * x ^ (seq_along(b) - 1)) -
-    callFun(fn, x)
+remRatFunc <- function(x, a, b) {
+  sum(a * x ^ (seq_along(a) - 1)) / sum(b * x ^ (seq_along(b) - 1))
 }
+
+remRatErr <- function(x, a, b, fn) {remRatFunc(x, a, b) - callFun(fn, x)}
 
 remRatRoots <- function(x, a, b, fn) {
   r <- double(length(x) - 1)
@@ -164,8 +172,7 @@ remRatSwitch <- function(r, l, u, a, b, fn, eps) {
   x
 }
 
-remRat <- function(fn, lower, upper, numerd, denomd, showProgress = FALSE,
-                   opts = list()) {
+remRat <- function(fn, lower, upper, numerd, denomd, opts = list()) {
 
   # Handle configuration options
   if ("maxiter" %in% names(opts)) {
@@ -174,10 +181,16 @@ remRat <- function(fn, lower, upper, numerd, denomd, showProgress = FALSE,
     maxiter <- 2500
   }
 
+  if ("showProgress" %in% names(opts)) {
+    maxiter <- opts$showProgress
+  } else {
+    showProgress <- FALSE
+  }
+
   if ("tol" %in% names(opts)) {
     tol <- opts$tol
   } else {
-    tol <- sqrt(.Machine$double.eps)
+    tol <- 1e-12
   }
 
   if ("eps" %in% names(opts)) {
@@ -190,13 +203,14 @@ remRat <- function(fn, lower, upper, numerd, denomd, showProgress = FALSE,
   x <- chebNodes(n, lower, upper)
 
   convergeErr <- function(x, fn, tol, nD, dD) {
-    E <- 0
+    E <- 1
     j <- 0
     repeat {
-      j <- j + 1L
+      j <- j + 1
+      if (j > maxiter / 100) break
       RR <- remRatCoeffs(x, E, fn, nD, dD)
-      if (isTRUE(all.equal(abs(E), RR$E, tol = tol))) break
-      E <- (abs(E) + RR$E) / 2
+      if (isTRUE(all.equal(abs(E), abs(RR$E), tol = tol))) break
+      E <- (E + RR$E) / 2
     }
     RR
   }
@@ -217,12 +231,12 @@ remRat <- function(fn, lower, upper, numerd, denomd, showProgress = FALSE,
     errs <- sapply(x, remRatErr, a = RR$a, b = RR$b, fn = fn)
     abserrs <- abs(errs)
     if (showProgress) message("i: ", i, " E: ", RR$E, " maxErr: ", max(abserrs),
-                              " Diff:", max(abserrs) - RR$E)
+                              " Diff:", max(abserrs) - abs(RR$E))
 
     if (all(diff(abserrs) < tol) &&
         all(abs(diff(sign(errs))) == 2) &&
-        (max(abserrs) <= RR$E ||
-         isTRUE(all.equal(max(abserrs), RR$E, tol = tol)))) break
+        (max(abserrs) <= abs(RR$E) ||
+         isTRUE(all.equal(max(abserrs), abs(RR$E), tol = tol)))) break
   }
   ret <- list(a = RR$a, b = RR$b, E = RR$E)
   attr(ret, "type") <- "Rational"
@@ -244,14 +258,18 @@ print.RatApprox <- function(x, ...) {
 
 plot.RatApprox <- function(x, ...) {
   rng <- attr(x, "range")
+  xF <- attr(x, "basis")
   z <- seq(rng[1], rng[2], length.out = 1000L)
 
   if (attr(x, "type") == "Polynomial") {
     zz <- sapply(z, remPolyErr, x$b, attr(x, "func"))
+    yF <- sapply(xF, remPolyErr, x$b, attr(x, "func"))
   } else {
     zz <- sapply(z, remRatErr, x$a, x$b, attr(x, "func"))
+    yF <- sapply(xF, remRatErr, x$a, x$b, attr(x, "func"))
   }
+
   plot(z, zz, type = 'l',  xlab = "x", ylab = "Error")
   abline(h = 0)
-  abline(v = attr(x, "basis"), col = "red")
+  points(xF, yF, col = "red", pch = 16)
 }
