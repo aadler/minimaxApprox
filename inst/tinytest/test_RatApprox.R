08df4f64b8a14c78bfaed465c13beb4a13775181
Avraham Adler <avraham.adler@mcgillpartners.com>
2023-07-02 10:31:52 +0300

    More tests (callFun and isOscil)

--- inst/tinytest/test_RatApprox.R
+++ inst/tinytest/test_RatApprox.R
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: MPL-2.0+
 
 tol <- 1e-7
+cFErr <- "Unable to parse function."
 
 # Test chebNodes
 n <- 6L
@@ -14,4 +15,19 @@ expect_equal(control, chebNodes(n, -1, 1), tolerance = tol)
 k <- 1:5
 control <- matrix(c(rep(1, 5L), k, k ^ 2, k ^ 3, k ^ 4, k ^ 5, k ^ 6), ncol = 7)
 expect_identical(control, vanderMat(k, 6))
+
+# Test callFun
+## Test functionality
+fn <- function(x) tan(x) - x ^ 3
+control <- tan(-0.4) - (-0.4) ^ 3
+expect_equal(control, callFun(fn, -0.4), tolerance = tol)
+
+## Test error trapping
+expect_error(callFun("x ^ 2", -0.4), cFErr)
+
+# Test isOscil
+control <- c(-2, 1, -3, 4, -1, 6, -7)
+expect_true(isOscil(control))
+control <- c(-2, 1, -3, 4, -1, -6)
+expect_false(isOscil(control))
 
