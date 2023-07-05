fca6aae8fbbca8807bb82f7e5e106fbedee0068d
Avraham Adler <avraham.adler@mcgillpartners.com>
2023-07-05 17:10:24 +0300

    1) Add Function description comments 2) Use "lengths" instead of sapply 3) Import lessons learned from Polynomial root finding and switches 4) Comment out oscillation test for now 5) Raise default maxiter to 3000 6) Allow for passing in starting x's 7) Test for denominator polynomial having root (0) in interval. 7a) As of now this trips an error. 7b) Commented out code which "moves" x's away from root but leaving it there for future experimentation 8) Make consistent return with Polynomial

--- R/RemezRational.R
+++ R/RemezRational.R
@@ -1,7 +1,7 @@
 # Copyright Avraham Adler (c) 2023
 # SPDX-License-Identifier: MPL-2.0+
 
-
+# Function to create augmented Vandermonde matrix
 remRatMat <- function(x, E, y, nD, dD) {
   n <- length(x)
   altSgn <- (-1) ^ (seq_len(n) - 1)
@@ -12,56 +12,109 @@ remRatMat <- function(x, E, y, nD, dD) {
   cbind(AMat, BMat, -altSgn, deparse.level = 0)
 }
 
+# Function to calculate coefficients given matrix and known values
 remRatCoeffs <- function(x, E, fn, nD, dD) {
   y <- callFun(fn, x)
   P <- solve(remRatMat(x, E, y, nD, dD), y)
   RR <- list(a = P[seq_len(nD + 1)],            # Works even if nD = 0
              b = c(1, P[seq_len(dD) + nD + 1]), # Works even if dD = 0
              E = P[length(P)])
-  if (sum(sapply(RR, length)) != (length(x) + 1)) {
+  if (sum(lengths(RR)) != (length(x) + 1)) {
     stop("Catastrophic Error. Result vector not of required length.")
   }
   RR
 }
 
+# Function to calculate value of minimax rational approximation at x given a & b
 remRatFunc <- function(x, a, b)  polyCalc(x, a) / polyCalc(x, b)
 
-remRatErr <- function(x, a, b, fn) {remRatFunc(x, a, b) - callFun(fn, x)}
+# Function to calculate error between known and calculated values
+remRatErr <- function(x, a, b, fn) remRatFunc(x, a, b) - callFun(fn, x)
 
+# Function to identify roots of the error equation for use as bounds in finding
+# the maxima and minima
 remRatRoots <- function(x, a, b, fn) {
-  r <- double(length(x) - 1)
+  if (all(abs(remRatErr(x, a, b, fn)) <= 5 * .Machine$double.eps)) {
+    stop("This code only functions to machine double precision. All error ",
+         "values are below machine double precision. Please try again using a ",
+         "lesser degree.")
+  }
+  r <- double(length(x) - 1L)
   for (i in seq_along(r)) {
-    r[i] <- uniroot(remRatErr, lower = x[i], upper = x[i + 1L],
-                    a = a, b = b, fn = fn)$root
+    intv <- c(x[i], x[i + 1L])
+    root <- tryCatch(uniroot(remRatErr, interval = intv, a = a, b = b, fn = fn),
+                     error = function(cond) simpleError(trimws(cond$message)))
+    # If there is no root in the interval, take the lower endpoint
+    if (inherits(root, "simpleError")) {
+      r[i] <- intv[which.min(abs(intv))]
+    } else {
+      r[i] <- root$root
+    }
   }
   r
 }
 
-remRatSwitch <- function(r, l, u, a, b, fn, eps) {
+# Function to identify new x positions. This algorithm uses the multi-switch
+# paradigm, not the single switch.
+remRatSwitch <- function(r, l, u, a, b, fn) {
   nodes <- data.frame(lower = c(l, r), upper = c(r, u))
-  x <- double(length(r) + 1)
+  x <- double(dim(nodes)[1L])
+  maximize <- sign(remRatErr(l, a, b, fn)) == 1
   for (i in seq_along(x)) {
-    x[i] <- optimize(remRatErr,
-                     lower = nodes$lower[i],
-                     upper = nodes$upper[i],
-                     a = a, b = b, fn = fn,
-                     maximum = remRatErr(nodes$lower[i] + eps, a = a, b = b,
-                                         fn = fn) > 0)[[1]]
+    intv <- unlist(nodes[i, ])
+    extrma <- tryCatch(optimize(remRatErr, interval = intv,
+                                a = a, b = b, fn = fn, maximum = maximize),
+                       error = function(cond) simpleError(trimws(cond$message)))
+
+    if (inherits(extrma, "simpleError")) {
+      endPtErr <- remRatErr(intv, a, b, fn)
+      if (maximize) {
+        x[i] <- intv[which.max(endPtErr)]
+      } else {
+        x[i] <- intv[which.min(endPtErr)]
+      }
+    } else {
+      x[i] <- extrma[[1L]]
+    }
+
+    # Test endpoints for max/min
+    p <- c(nodes$lower[i], x[i], nodes$upper[i])
+    testDF <- data.frame(p = p, E = remRatErr(p, a, b, fn))
+    if (maximize) {
+      x[i] <- testDF$p[which.max(testDF$E)]
+    } else {
+      x[i] <- testDF$p[which.min(testDF$E)]
+    }
+
+    # Flip maximize
+    maximize <- !maximize
   }
+
+  # Test Oscillation
+  # if (!isOscil(remRatErr(x, a, b, fn))) {
+  #   stop("Control points do not result in oscillating errors.")
+  # }
   x
 }
 
-remRat <- function(fn, lower, upper, numerd, denomd, opts = list()) {
+# Main function to calculate and return the minimax rational approximation
+remRat <- function(fn, lower, upper, numerd, denomd, xi = NULL, opts = list()) {
 
   # Handle configuration options
   if ("maxiter" %in% names(opts)) {
-    maxiter <- opts$maxit
+    maxiter <- opts$maxiter
   } else {
-    maxiter <- 2500
+    maxiter <- 3000L
+  }
+
+  if ("miniter" %in% names(opts)) {
+    miniter <- opts$miniter
+  } else {
+    miniter <- 15L
   }
 
   if ("showProgress" %in% names(opts)) {
-    maxiter <- opts$showProgress
+    showProgress <- opts$showProgress
   } else {
     showProgress <- FALSE
   }
@@ -69,26 +122,31 @@ remRat <- function(fn, lower, upper, numerd, denomd, opts = list()) {
   if ("tol" %in% names(opts)) {
     tol <- opts$tol
   } else {
-    tol <- 1e-12
+    tol <- 5 * .Machine$double.eps
   }
 
-  if ("eps" %in% names(opts)) {
-    tol <- opts$eps
+  # Initial x's
+  if (is.null(xi)) {
+    x <- chebNodes(numerd + denomd + 2, lower, upper)
   } else {
-    eps <- min((upper - lower) / 1000, 1e-3)
+    x <- xi
+    if (length(xi) != numerd + denomd + 2) {
+      stop("Given the requested degrees for numerator and denominator, ",
+           "the x-vector needs to have ", numerd + denomd + 2, " elements.")
+    }
   }
 
-  n <- numerd + denomd + 2
-  x <- chebNodes(n, lower, upper)
-
+  # Since E is initially a guess we need to iterate solving the system of
+  # equations until E converges. This function has no other use so defined
+  # INSIDE the remRat call.
   convergeErr <- function(x, fn, tol, nD, dD) {
     E <- 1
-    j <- 0
+    j <- 0L
     repeat {
-      j <- j + 1
-      if (j > maxiter / 100) break
+      j <- j + 1L
+      if (j > maxiter / 100) break  # Otherwise this would take FOREVER
       RR <- remRatCoeffs(x, E, fn, nD, dD)
-      if (isTRUE(all.equal(abs(E), abs(RR$E), tol = tol))) break
+      if (all(abs(abs(E) - abs(RR$E)) < tol)) break
       E <- (E + RR$E) / 2
     }
     RR
@@ -97,33 +155,58 @@ remRat <- function(fn, lower, upper, numerd, denomd, opts = list()) {
   RR <- convergeErr(x, fn, tol, numerd, denomd)
   i <- 0L
   repeat {
-    if (i > maxiter) {
-      message("Convergence not acheived in ", maxiter, " iterations.")
-      break
-    }
-
     i <- i + 1L
     r <- remRatRoots(x, RR$a, RR$b, fn)
-    x <- remRatSwitch(r, lower, upper, RR$a, RR$b, fn, eps)
+    x <- remRatSwitch(r, lower, upper, RR$a, RR$b, fn)
+    # if (!is.null(dngr)) {
+    #   xrat <- x / dngr
+    #   closebelow <- which(xrat <= 1 & xrat > 0.99)
+    #   closeabove <- which(xrat > 1 & xrat <= 1.01)
+    #   x[closebelow] <- x[closebelow] * 0.9
+    #   x[closeabove] <- x[closeabove] * 1.1
+    #   x <- sort(x)
+    # }
     RR <- convergeErr(x, fn, tol, numerd, denomd)
-
-    errs <- sapply(x, remRatErr, a = RR$a, b = RR$b, fn = fn)
-    abserrs <- abs(errs)
+    dngr <- checkDenom(RR$b, lower, upper)
+    if (!is.null(dngr)) {
+      stop("The ", denomd, " degree polynomial in the denominator has a zero ",
+           "at ", fN(dngr), " which makes rational approximation perilous for ",
+           "this function over the interval [", lower, ", ", upper, "].")
+    }
+    errs <- remRatErr(x, a = RR$a, b = RR$b, fn = fn)
     mxae <- max(abs(errs))
-    if (showProgress) message("i: ", i, " E: ", RR$E, " maxErr: ", max(abserrs),
-                              " Diff:", max(abserrs) - abs(RR$E))
 
-    if (all(diff(abserrs) < tol) &&
-        all(abs(diff(sign(errs))) == 2) &&
-        (max(abserrs) <= abs(RR$E) ||
-         isTRUE(all.equal(max(abserrs), abs(RR$E), tol = tol)))) break
+    if (showProgress) {
+      message("i: ", i, " E: ", fN(RR$E), " maxErr: ", fN(mxae),
+              " Diff:", fN(abs(mxae - abs(RR$E))))
+    }
+
+    if ((isConverged(errs, RR$E, tol) && i >= miniter) || i > maxiter) break
   }
-  ret <- list(a = RR$a, b = RR$b, EE = RR$E, OE = mxae, iterations = i,
-              basis = x)
+
+  gotWarning <- FALSE
+
+  if (i >= maxiter) {
+    mess <- paste("Convergence not acheived in", maxiter, "iterations.\n")
+    mess <- paste0(mess, "Maximum observed error ",
+                   formatC(mxae / RR$E, digits = 6L), " times expected.")
+    warning(mess)
+    gotWarning <- TRUE
+  }
+
+  if (all(abs(errs) < 10 * .Machine$double.eps)) {
+    warning("All errors very near machine double precision. The solution may ",
+            "not be optimal but should be best given the desired precision ",
+            "and floating point limitations. Try a lower degree if needed.")
+    gotWarning <- TRUE
+  }
+
+  ret <- list(a = RR$a, b = RR$b, EE = abs(RR$E), OE = mxae,
+              Diff = abs(abs(RR$E) - mxae), iterations = i, x = x,
+              Warning = gotWarning)
   attr(ret, "type") <- "Rational"
   attr(ret, "func") <- fn
   attr(ret, "range") <- c(lower, upper)
   class(ret) <- c("RatApprox", class(ret))
   ret
 }
-
