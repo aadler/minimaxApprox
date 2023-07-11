749f3cce307f56415078c1b0f99fd8edfb1f0b6d
Avraham Adler <avraham.adler@mcgillpartners.com>
2023-07-11 21:03:59 +0300

    Cleaned up and made integers explicit

--- inst/tinytest/test_MiniMaxApprox.R
+++ inst/tinytest/test_MiniMaxApprox.R
@@ -5,25 +5,25 @@ tol <- 1e-7
 
 # Test other components of minimaxApprox - Poly that have not been tested before
 fn <- function(x) ifelse(abs(x) < 1e-20, 1, sin(x) / x)
-PP <- minimaxApprox(fn, -1, 1, 9, errType = "rel",
-                    opts = list(maxiter = 100, convRatio = 1.5))
+PP <- minimaxApprox(fn, -1, 1, 9L, opts = list(maxiter = 100L, convRatio = 1.5))
 expect_false(PP$Warning)
 
 # Should show at least one line of output due to show progress
-expect_message(minimaxApprox(fn, -1, 1, 9,
+expect_message(minimaxApprox(fn, -1, 1, 9L,
                              opts = list(miniter = 0L, showProgress = TRUE)),
                "i: 1 E: ")
 
 fn <- function(x) sin(x) + cos(x)
-expect_warning(minimaxApprox(fn, -1, 1, 13),
+expect_warning(minimaxApprox(fn, -1, 1, 13L),
                "All errors very near machine double precision.")
-expect_warning(minimaxApprox(fn, -1, 1, 9, opts = list(maxiter = 0)),
-               "Convergence not acheived in ")
 
-PP <- suppressWarnings(minimaxApprox(fn, -1, 1, 13, opts = list(tol = 1e-14)))
+expect_warning(minimaxApprox(fn, -1, 1, 9L, opts = list(maxiter = 1L)),
+               "Convergence to requested ratio and tolerance not acheived in")
+
+PP <- suppressWarnings(minimaxApprox(fn, -1, 1, 13L, opts = list(tol = 1e-14)))
 expect_true(PP$Warning)
 
-expect_warning(minimaxApprox(fn, -1, 1, 10, xi = 6),
+expect_warning(minimaxApprox(fn, -1, 1, 10L, xi = 6),
                "Polynomial approximation uses Chebyeshev nodes for initial")
 
 # Test other components of minimaxApprox - Rat that have not been tested before
@@ -38,31 +38,31 @@ expect_message(minimaxApprox(fn, -1, 1, dg, opts = list(miniter = 2L,
 
 dg <- c(3L, 3L)
 # Test error message
-expect_error(minimaxApprox(fn, -1, 1, dg, xi = chebNodes(5, -1, 1)),
+expect_error(minimaxApprox(fn, -1, 1, dg, xi = chebNodes(5L, -1, 1)),
              "Given the requested degrees for numerator and denominator")
 
 expect_error(minimaxApprox(fn, -1, 1, 1:3),
              "Polynomial approximation takes one value for degree and")
 
 # Test warning flag
-expect_true(suppressWarnings(minimaxApprox(fn, -1, 1, c(5, 4),
+expect_true(suppressWarnings(minimaxApprox(fn, -1, 1, c(5L, 4L),
                                            opts = list(maxiter = 6L))$Warning))
 
 # Test informational message
 fn <- function(x) exp(x) - 1
-expect_message(minimaxApprox(fn, -0.15, 0.15, c(3, 4)),
+expect_message(minimaxApprox(fn, -0.15, 0.15, c(3L, 4L)),
                "All errors very near machine double precision. The solution")
 
 # Test passing some parameters
-expect_silent(minimaxApprox(fn, -0.15, 0.15, c(2, 2),
+expect_silent(minimaxApprox(fn, -0.15, 0.15, c(2L, 2L),
                             opts = list(convRatio = 1.05, tol = 1e-4)))
 
 # Test denominator error message
-expect_error(minimaxApprox(tan, 1, 2, c(2, 3)),
+expect_error(minimaxApprox(tan, 1, 2, c(2L, 3L)),
              "The 3 degree polynomial in the denominator has a zero at 1.57")
 
 # Test print, plot, and coef methods
-PP <- minimaxApprox(function(x) exp(x), 0, 1, 5, "abs")
+PP <- minimaxApprox(function(x) exp(x), 0, 1, 5L, "abs")
 expect_identical(unlist(coef(PP), use.names = FALSE), PP$a)
 expect_stdout(print(PP))
 expect_stdout(plot(PP))
