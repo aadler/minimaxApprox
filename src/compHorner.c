507df287fc7ce780b62b89c11cb226168ed5c297
Avraham Adler <avraham.adler@gmail.com>
2023-08-21 12:56:43 -0400

    Review code for efficiecies and greatly expand comments.

--- src/compHorner.c
+++ src/compHorner.c
@@ -7,12 +7,22 @@
 #include <Rmath.h>
 #include <R_ext/Rdynload.h>
 
+// Based on Langlois et al.(2006)
+// https://drops.dagstuhl.de/opus/volltexte/2006/442/
+
 // Most of the _c functions were used while developing the C code. In actuality,
 // they should never be called from R as it is faster to use C as much as
 // possible. Since there is no good way to pass list results to C functions, the
 // core functionality has been rewritten as non-SEXP C and only the "outermost"
 // calls will be exposed. The code will be commented/nocoved out, but left in
-// the files for pedagogic, debugging, and reminder reasons.
+// the files for pedagogic, debugging, and reminder reasons. This is why the
+// eftHorner_c and hornerSum_c may have optimzations/efficiencies not found in
+// the "component" functions.
+//
+// Also, many of the variables need to be initialized as "volatile" as we
+// specifically DO NOT WANT the compiler to optimize them out. The entire point
+// of EFT algorithms is to capture the floating-point error as best possible!
+//
 // (AA: 2023-08-21)
 
 // # nocov start
@@ -157,39 +167,57 @@ extern SEXP eftHorner_c(SEXP x, SEXP a) {
   const int m = LENGTH(x);
   const int n = LENGTH(a);
   const int nm1 = n - 1;
+  // s0 is the "older" vector of sums, This is also the "val" that is returned
+  // to R so it needs to be an SEXP.
   SEXP s0 = PROTECT(allocVector(REALSXP, m));
-  SEXP s1 = PROTECT(allocVector(REALSXP, m));
+  // s1 is the "newer" vector of sums. It does not need to be an SEXP as it
+  // never leaves C to go to R.
+  double s1[m];
+  // This is the pi Matrix which is returned to R.
   SEXP piM = PROTECT(allocMatrix(REALSXP, nm1, m));
+  // This is the sigma Matrix which is returned to R.
   SEXP sigM = PROTECT(allocMatrix(REALSXP, nm1, m));
-  SEXP val = PROTECT(allocVector(REALSXP, m));
+  // This is the return object (an R list).
   SEXP ret = PROTECT(allocVector(VECSXP, 3));
+  // This holds the names of the compoments of the return object so it too must
+  // be an SEXP.
   SEXP names = PROTECT(allocVector(STRSXP, 3));
+  // Set the names.
   SET_STRING_ELT(names, 0, mkChar("val"));
   SET_STRING_ELT(names, 1, mkChar("pi"));
   SET_STRING_ELT(names, 2, mkChar("sig"));
 
+  // Per Hadley's suggestion to create helper pointer variable once
+  // http://adv-r.had.co.nz/C-interface.html#c-vectors - Accessing vector data
   double *px = REAL(x);
   double *pa = REAL(a);
   double *ps0 = REAL(s0);
-  double *ps1 = REAL(s1);
-  double *pval = REAL(val);
   double *ppiM = REAL(piM);
   double *psigM = REAL(sigM);
+  // The first element of the twoProd is used more than once so save to Ax.
+  // See the R code for equivalent.
   volatile double Ax;
 
+  // Since we will return 0 if n = 0---which is technically legal if asking for
+  // the best constant estimate or using rational to test polynomial---we must
+  // initialize the s0 vector and the pi and sigma matrices with 0.
+
   memset(ps0, 0, m * sizeof(double));
-  memset(ps1, 0, m * sizeof(double));
-  memset(pval, 0, m * sizeof(double));
   memset(ppiM, 0, m * nm1 * sizeof(double));
   memset(psigM, 0, m * nm1 * sizeof(double));
 
+  // If n is at least 1, initialize s0 with the last value of a.
   if (n > 0) {
     for (int i = 0; i < m; ++i) {
       ps0[i] = pa[nm1];
     }
   }
   if (n > 1) {
-  // Key here is to remember R stores matrices as contiguous vectors in
+  // If n > 1, we need to use the Compensated Horner scheme to evaluate the
+  // polynomial. The algorithm will follow Langlois et al.(2006), which as a
+  // Horner method, starts at the end and works backwards.
+  //
+  // The key here is to remember R stores matrices as contiguous vectors in
   // COLUMN-MAJOR order. So to get to cell [i, j]---assuming C convention that
   // everything starts at 0---you start by iterating over COLUMNS first and then
   // actually want (numrows * rowIndex + colIndex). See the R code for how it
@@ -206,14 +234,18 @@ extern SEXP eftHorner_c(SEXP x, SEXP a) {
   // (AA: 2023-08-20)
     for (int j = nm1; j-- > 0; ) {
       for (int i = 0; i < m; ++i) {
+        // First element of twoProd
         Ax = ps0[i] * px[i];
+        // Second element of twoProd
         ppiM[nm1 * i + j] = twoPrody(ps0[i], px[i]);
-        ps1[i] = Ax + pa[j];
+        // First element of twoSum
+        s1[i] = Ax + pa[j];
+        // Second element of twoSum
         psigM[nm1 * i + j] = twoSumy(Ax, pa[j]);
       }
   // Storing the new as old.
       for (int i = 0; i < m; ++i) {
-        ps0[i] = ps1[i];
+        ps0[i] = s1[i];
       }
     }
   }
@@ -222,61 +254,62 @@ extern SEXP eftHorner_c(SEXP x, SEXP a) {
   SET_VECTOR_ELT(ret, 1, piM);
   SET_VECTOR_ELT(ret, 2, sigM);
   setAttrib(ret, R_NamesSymbol, names);
-  UNPROTECT(7);
+  UNPROTECT(5);
   return(ret);
 }
 
 extern SEXP hornerSum_c(SEXP x, SEXP p, SEXP np, SEXP q, SEXP nq) {
   const int m = LENGTH(x);
-  int *prows = INTEGER(np);
-  int *qrows = INTEGER(nq);
+  const int prows = INTEGER(np)[0];
+  const int qrows = INTEGER(nq)[0];
+  // Length of the "flattened" matrix. Will be used for error checking.
+  const int lp = LENGTH(p);
 
-  SEXP ret = PROTECT(allocVector(REALSXP, m));
-
-  double *pret = REAL(ret);
-  memset(pret, 0, m * sizeof(double));
-
-  if (prows[0] <= 0) {
-    UNPROTECT(1);
-    return(ret);
-  }
-
-  if (prows[0] != qrows[0]) {
-    error("Error polynomials must be of same length.");
-  }
-
-  int pcols = LENGTH(p) / prows[0];
-  if (pcols != m) {
-    error("Polynomials must have same length as x.");
-  }
-
-  double *px = REAL(x);
-  double *pp = REAL(p);
-  double *pq = REAL(q);
-
+  // r0 is returned regardles, so declare as an SEXP and initialize here.
   SEXP r0 = PROTECT(allocVector(REALSXP, m));
-  SEXP r1 = PROTECT(allocVector(REALSXP, m));
   double *pr0 = REAL(r0);
-  double *pr1 = REAL(r1);
+  memset(pr0, 0, m * sizeof(double));
 
-  // See above for why this addressing schema is needed.
-  for (int i = 0; i < m; ++i) {
-    pr0[i] = pp[prows[0] * (i + 1) - 1] +  pq[prows[0] * (i + 1) - 1];
-  }
+  // prows can be 0 if asking for constant. Ignoring size issues in this case, which
+  // may be a mistake. Only asking/continuing if prows > 0.
+  if (prows > 0) {
+    if (lp != LENGTH(q)) {
+      error("Error polynomials must be of same dimension.");
+    }
 
-  // See above for why this addressing schema is needed.
-  if (prows[0] > 1) {
-    int nm1 = prows[0] - 1;
-    for (int j = nm1; j-- > 0; ) {
-      for (int i = 0; i < m; ++i) {
-        pr1[i] = pr0[i] * px[i] + pp[prows[0] * i + j] + pq[prows[0] * i + j];
-      }
-      for (int i = 0; i < m; ++i) {
-        pr0[i] = pr1[i];
+    if (lp / prows != m) {
+      error("Polynomials must have same length as x.");
+    }
+
+    // If we got here, then prows > 0 and data is of proper size.
+    double *px = REAL(x);
+    double *pp = REAL(p);
+    double *pq = REAL(q);
+    // "Newer" sum vector, similar to s1 in eftHorner.
+    double r1[m];
+
+    // See above for why this addressing schema is needed.
+    // Replace the 0's with the sum of the LAST rows of p and q (pi and Sigma).
+    for (int i = 0; i < m; ++i) {
+      pr0[i] = pp[prows * (i + 1) - 1] +  pq[prows * (i + 1) - 1];
+    }
+
+    // See above for why this addressing schema is needed.
+    // Now build "upwards".
+    if (prows > 1) {
+      int nm1 = prows - 1;
+      for (int j = nm1; j-- > 0; ) {
+        for (int i = 0; i < m; ++i) {
+          r1[i] = pr0[i] * px[i] + (pp[prows * i + j] + pq[prows * i + j]);
+        }
+        for (int i = 0; i < m; ++i) {
+          pr0[i] = r1[i];
+        }
       }
     }
   }
-  UNPROTECT(3);
+
+  UNPROTECT(1);
   return(r0);
 }
 
