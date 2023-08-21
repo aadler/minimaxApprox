3a0f686611055079ed46dadce834b0279f8a8ddb
Avraham Adler <avraham.adler@gmail.com>
2023-08-21 01:55:52 -0400

    Update unit tests for new Compensated Horner Scheme algos.

--- inst/tinytest/test_horner.R
+++ inst/tinytest/test_horner.R
@@ -4,9 +4,14 @@
 tol <- 1e-7
 x <- 1:3
 a <- c(1, 1e-6, 1e-12)
-expect_equal(minimaxApprox:::horner(x, a),
+aa <- matrix(rep(a, 2), ncol = 3)
+expect_equal(minimaxApprox:::hornerC(x, a),
              minimaxApprox:::compensatedHorner(x, a),
              tolerance = tol)
 
-expect_error(minimaxApprox:::hornerSum(x, a, c(a, 1)),
+expect_error(minimaxApprox:::hornerSumC(x, a, c(a, 1)),
              "Error polynomials must be of same length.")
+
+expect_silent(minimaxApprox:::hornerSumC(x, aa, aa))
+expect_error(minimaxApprox:::hornerSumC(x[-1], aa, aa),
+             "Polynomials must have same length as x.")
