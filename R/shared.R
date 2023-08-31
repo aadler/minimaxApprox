9a6ba8b4a274fd88b1a47d372bfc6c2c813bc0c3
Avraham Adler <avraham.adler@gmail.com>
2023-08-31 18:53:17 -0400

    np1 ==> n plus 1

--- R/shared.R
+++ R/shared.R
@@ -14,8 +14,8 @@ chebNodes <- function(n, a, b) {
 
 # Create Vandermonde matrix to a polynomial degree n, or n + 1 terms
 vanderMat <- function(x, n) {
-  n1 <- n + 1L
-  matrix(rep(x, each = n1) ^ (seq_len(n1) - 1L), ncol = n1, byrow = TRUE)
+  np1 <- n + 1L
+  matrix(rep(x, each = np1) ^ (seq_len(np1) - 1L), ncol = np1, byrow = TRUE)
 }
 
 # Call the function being approximated on the points x
