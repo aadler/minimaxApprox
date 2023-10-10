7d7b6709fe0bd240dcda194818ad2b61b3989bb6
Avraham Adler <avraham.adler@gmail.com>
2023-10-09 23:15:35 -0400

    80 char length

--- inst/tinytest/test_MiniMaxApprox.R
+++ inst/tinytest/test_MiniMaxApprox.R
@@ -169,9 +169,9 @@ if (!("darwin" %in% tolower(Sys.info()[["sysname"]]))) {
   ## Test successful restart
   ## Below also tests the failover to QR
   mess <- paste("The algorithm failed while looking for a polynomial of degree",
-                "10 but successfully completed when looking for a polynomial of",
-                "degree 11 with the largest coefficient's contribution to the",
-                "approximation <= the tailtol option. The result is a",
+                "10 but successfully completed when looking for a polynomial",
+                "of degree 11 with the largest coefficient's contribution to",
+                "the approximation <= the tailtol option. The result is a",
                 "polynomial of length 10 as the uppermost coefficient is",
                 "effectively 0.")
   fn <- function(x) 1 / (1 + (5 * x) ^ 2)
