f093cf927d9e94e7cd7035e0c3e8c4ad4a74c820
Avraham Adler <avraham.adler@gmail.com>
2024-01-30 15:17:09 -0500

    Start working on using Chebyshev basis. Vectorized function to calculate Chebyshev polynomials of a given order k and add unit test.

---
+++ R/Chebyshev.R
@@ -0,0 +1,32 @@
+# Copyright Avraham Adler (c) 2024
+# SPDX-License-Identifier: MPL-2.0+
+
+# Recursive function to calculate the kth Chebyshev polynomial T_k. Eventually,
+# this should be split into pre-calculated known use cases and ported to C for
+# speed, but for development purposes, keeping everything in R. The original is
+# the recursive definition. Using the cos/cosh based version for speed.
+
+# chebPoly <- function(x, k, maxExpo = 40L) {
+#   k <- as.integer(k)
+#   stopifnot(k >= 0,
+#             k <= maxExpo,
+#             is.numeric(x)
+#   )
+#   if (k == 0L) {
+#     return(1)
+#   } else if (k == 1L) {
+#     return(x)
+#   } else {
+#     return(2 * x * chebPoly(x, k - 1L) - chebPoly(x, k - 2L))
+#   }
+# }
+
+chebPoly <- function(x, k) {
+  k <- as.integer(k)
+  stopifnot(k >= 0, is.numeric(x))
+  suppressWarnings(ifelse(x <= -1,
+                          (-1) ^ k * cosh(k * acosh(-x)),
+                          ifelse(x <= 1L,
+                                 cos(k * acos(x)),
+                                 cosh(k * acosh(x)))))
+}
---
+++ inst/tinytest/test_Chebyshev.R
@@ -0,0 +1,39 @@
+# Copyright Avraham Adler (c) 2024
+# SPDX-License-Identifier: MPL-2.0+
+
+tol <- sqrt(.Machine$double.eps)
+
+# See https://mathworld.wolfram.com/ChebyshevPolynomialoftheFirstKind.html
+
+x <- 3
+
+# Test chebPoly
+## Scalar
+expect_equal(minimaxApprox:::chebPoly(x, 0), 1, tolerance = tol)
+expect_equal(minimaxApprox:::chebPoly(x, 1), x, tolerance = tol)
+expect_equal(minimaxApprox:::chebPoly(x, 2), 2 * x ^ 2 - 1, tolerance = tol)
+expect_equal(minimaxApprox:::chebPoly(x, 3), 4 * x ^ 3 - 3 * x, tolerance = tol)
+expect_equal(minimaxApprox:::chebPoly(x, 4), 8 * x ^ 4 - 8 * x ^ 2 + 1,
+             tolerance = tol)
+expect_equal(minimaxApprox:::chebPoly(x, 5), 16 * x ^ 5 - 20 * x ^ 3 + 5 * x,
+             tolerance = tol)
+expect_equal(minimaxApprox:::chebPoly(x, 6),
+             32 * x ^ 6 - 48 * x ^ 4 + 18 * x ^ 2 - 1, tolerance = tol)
+
+## Vector and in all three zones
+x <- c(-10, -2, -0.5, -1e-10, 0, 1e-10, 0.5, 2, 10)
+expect_equal(minimaxApprox:::chebPoly(x, 0), rep(1, length(x)), tolerance = tol)
+expect_equal(minimaxApprox:::chebPoly(x, 1), x, tolerance = tol)
+expect_equal(minimaxApprox:::chebPoly(x, 2), 2 * x ^ 2 - 1, tolerance = tol)
+expect_equal(minimaxApprox:::chebPoly(x, 3), 4 * x ^ 3 - 3 * x, tolerance = tol)
+expect_equal(minimaxApprox:::chebPoly(x, 4), 8 * x ^ 4 - 8 * x ^ 2 + 1,
+             tolerance = tol)
+expect_equal(minimaxApprox:::chebPoly(x, 5), 16 * x ^ 5 - 20 * x ^ 3 + 5 * x,
+             tolerance = tol)
+expect_equal(minimaxApprox:::chebPoly(x, 6),
+             32 * x ^ 6 - 48 * x ^ 4 + 18 * x ^ 2 - 1, tolerance = tol)
+
+## Error trapping
+x <- c(3, "A")
+expect_error(minimaxApprox:::chebPoly(x, 3) , "is.numeric\\(x\\) is not TRUE")
+expect_error(minimaxApprox:::chebPoly(3, -1), "k >= 0 is not TRUE")
