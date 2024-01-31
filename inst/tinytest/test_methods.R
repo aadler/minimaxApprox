27c1c2e04aaa9a2e273dd034867f9d5136d908db
Avraham Adler <avraham.adler@gmail.com>
2024-01-31 14:58:06 -0500

    Add print unit tests for Chebyshev-based calls.

--- inst/tinytest/test_methods.R
+++ inst/tinytest/test_methods.R
@@ -12,6 +12,11 @@ expect_identical(unlist(coef(PP), use.names = FALSE), PP$a)
 expect_stdout(print(PP))
 expect_stdout(plot(PP))
 
+PP <- minimaxApprox(function(x) exp(x), 0, 1, 5L, TRUE, "c")
+expect_identical(unlist(coef(PP), use.names = FALSE), PP$a)
+expect_stdout(print(PP))
+expect_stdout(plot(PP))
+
 RR <- minimaxApprox(function(x) exp(x), 0, 1, c(2L, 2L))
 expect_identical(unlist(coef(RR)$a, use.names = FALSE), RR$a)
 expect_identical(unlist(coef(RR)$b, use.names = FALSE), RR$b)
@@ -25,6 +30,12 @@ expect_identical(unlist(coef(RR)$b, use.names = FALSE), RR$b)
 expect_stdout(print(RR))
 expect_stdout(plot(RR))
 expect_stdout(plot(RR, ylim = c(-5e-6, 5e-6)))
+
+RR <- suppressWarnings(minimaxApprox(exp, 0, 1, c(2L, 2L), TRUE, "c"))
+expect_identical(unlist(coef(RR)$a, use.names = FALSE), RR$a)
+expect_identical(unlist(coef(RR)$b, use.names = FALSE), RR$b)
+expect_stdout(print(RR))
+expect_stdout(plot(RR))
 
 # Test ignore y message
 mess <- paste("The y values are taken from the minimaxApprox object.",
