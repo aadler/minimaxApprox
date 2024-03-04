31128ce5df8eff17ada6b125c840d05ae59afe17
Avraham Adler <avraham.adler@mcgillpartners.com>
2024-03-04 20:38:09 +0200

    conviter can overwrite miniter too.

--- R/MiniMaxApprox.R
+++ R/MiniMaxApprox.R
@@ -21,8 +21,10 @@ minimaxApprox <- function(fn, lower, upper, degree, relErr = FALSE,
   }
 
   if ("conviter" %in% nopts) {
-    # If actually passed then overwrite maxiter if conviter > maxiter.
+    # If actually passed then overwrite both maxiter and miniter if conviter is
+    # greather than either one.
     opts$maxiter <- max(opts$maxiter, opts$conviter)
+    opts$miniter <- max(opts$miniter, opts$conviter)
   } else {
     opts$conviter <- 30L
   }
