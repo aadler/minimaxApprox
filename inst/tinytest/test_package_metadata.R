be19f6ab30967430f9f9792eaab86c8cfddeea95
Avraham Adler <avraham.adler@gmail.com>
2024-05-20 15:34:07 -0400

    Only test at home. rhub valgrind complains and it should not affect covr.

--- inst/tinytest/test_package_metadata.R
+++ inst/tinytest/test_package_metadata.R
@@ -1,22 +1,26 @@
 # Copyright Avraham Adler (c) 2023
 # SPDX-License-Identifier: MPL-2.0+
 
-pV <- packageVersion("minimaxApprox")
-cit <- toBibtex(citation("minimaxApprox"))
-nws <- news(package = "minimaxApprox")
-myOtherPkgs <- c("Delaporte", "lamW", "Pade", "revss", "MBBEFDLite")
+# Only test at home. rhub valgrind complains and it should not affect covr.
 
-# Test CITATION has most recent package version
-expect_true(any(grepl(pV, cit), fixed = TRUE))
+if (Sys.info()["nodename"] == "HOME") {
+  pV <- packageVersion("minimaxApprox")
+  cit <- toBibtex(citation("minimaxApprox"))
+  nws <- news(package = "minimaxApprox")
+  myOtherPkgs <- c("Delaporte", "lamW", "Pade", "revss", "MBBEFDLite")
 
-# Test NEWS has most recent package version
-expect_true(any(grepl(pV, nws, fixed = TRUE)))
+  # Test CITATION has most recent package version
+  expect_true(any(grepl(pV, cit), fixed = TRUE))
 
-# Test that NEWS has an entry with DESCRIPTION's Date
-expect_true(any(grepl(packageDate("minimaxApprox"), nws, fixed = TRUE)))
+  # Test NEWS has most recent package version
+  expect_true(any(grepl(pV, nws, fixed = TRUE)))
 
-# Test that CITATION doesn't contain the name of any other of my packages
-expect_false(any(sapply(myOtherPkgs, grepl, x = cit, fixed = TRUE)))
+  # Test that NEWS has an entry with DESCRIPTION's Date
+  expect_true(any(grepl(packageDate("minimaxApprox"), nws, fixed = TRUE)))
 
-# Test that NEWS doesn't contain the name of any other of my packages
-expect_false(any(sapply(myOtherPkgs, grepl, x = nws, fixed = TRUE)))
+  # Test that CITATION doesn't contain the name of any other of my packages
+  expect_false(any(sapply(myOtherPkgs, grepl, x = cit, fixed = TRUE)))
+
+  # Test that NEWS doesn't contain the name of any other of my packages
+  expect_false(any(sapply(myOtherPkgs, grepl, x = nws, fixed = TRUE)))
+}
