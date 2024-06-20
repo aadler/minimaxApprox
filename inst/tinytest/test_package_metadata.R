9ccab5ad06fd4a2b3c878ab6a7d188fce552112e
Avraham Adler <avraham.adler@gmail.com>
2024-06-20 02:29:53 -0400

    More robust metadata checking.

--- inst/tinytest/test_package_metadata.R
+++ inst/tinytest/test_package_metadata.R
@@ -4,10 +4,22 @@
 # Only test at home. rhub valgrind complains and it should not affect covr.
 
 if (Sys.info()["nodename"] == "HOME") {
-  pV <- packageVersion("minimaxApprox")
-  cit <- toBibtex(citation("minimaxApprox"))
-  nws <- news(package = "minimaxApprox")
-  myOtherPkgs <- c("Delaporte", "lamW", "Pade", "revss", "MBBEFDLite")
+
+  # Setup
+  myPkgs <- c("Delaporte",
+              "lamW",
+              "Pade",
+              "revss",
+              "minimaxApprox",
+              "MBBEFDLite")
+  thisPkg <- "minimaxApprox"
+  otrPkgs <- setdiff(myPkgs, thisPkg)
+
+  # Universal code
+  pV <- packageVersion(thisPkg)
+  pD <- packageDate(thisPkg)
+  cit <- toBibtex(citation(thisPkg))
+  nws <- news(package = thisPkg)
 
   # Test CITATION has most recent package version
   expect_true(any(grepl(pV, cit), fixed = TRUE))
@@ -16,11 +28,11 @@ if (Sys.info()["nodename"] == "HOME") {
   expect_true(any(grepl(pV, nws, fixed = TRUE)))
 
   # Test that NEWS has an entry with DESCRIPTION's Date
-  expect_true(any(grepl(packageDate("minimaxApprox"), nws, fixed = TRUE)))
+  expect_true(any(grepl(pD, nws, fixed = TRUE)))
 
   # Test that CITATION doesn't contain the name of any other of my packages
-  expect_false(any(sapply(myOtherPkgs, grepl, x = cit, fixed = TRUE)))
+  expect_false(any(sapply(otrPkgs, grepl, x = cit, fixed = TRUE)))
 
   # Test that NEWS doesn't contain the name of any other of my packages
-  expect_false(any(sapply(myOtherPkgs, grepl, x = nws, fixed = TRUE)))
+  expect_false(any(sapply(otrPkgs, grepl, x = nws, fixed = TRUE)))
 }
